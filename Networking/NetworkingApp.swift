9a587057876aa8839c68ec5ef618adfefeb38a42
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-10-14 22:23:50 +0900

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by 神山賢太郎 on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
